0d03fcf2253991d92839024b4f61f74aa5e98e53
krishnendu Dakshi <kdakshi2020@gamil.com>
2022-05-01 13:13:00 +0530

    added 4b.c

---
+++ ES CS 201/4b.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int i = 1, j = 2, k = 3;
+
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= 3; j++)
+        {
+            for (int k = 1; k <= 3; k++)
+            {
+                if (i != j && i != k && j != k)
+                {
+                    printf("%d%d%d", i, j, k);
+                    printf("\n");
+                }
+            }
+        }
+    }
+    return 0;
+}
